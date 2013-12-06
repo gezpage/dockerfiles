052afe1e897118a272d27730755c99088250f4d1
Gez Page <gezpage@gmail.com>
2013-12-06 12:29:12 +0000

    Updated notes

--- build_all.sh
+++ build_all.sh
@@ -12,6 +12,9 @@ docker build -t gezpage/redis redis
 docker build -t gezpage/symfony2:5.3 symfony2
 docker build -t gezpage/symfony2:5.5 symfony2.php55
 
+echo "Cleaning old containers"
+docker rm `docker ps -a -q`
+
 docker images
 
 echo "Finished building docker images"
