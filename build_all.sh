2dc397571fd39043e793d723b96fd59534f9385a
Gez Page <gezpage@gmail.com>
2013-12-06 12:22:08 +0000

    build all script

---
+++ build_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Building all images with gezpage namespace prefix"
+
+docker build -t gezpage/ubuntu ubuntu
+docker build -t gezpage/django django
+docker build -t gezpage/mongodb mongodb
+docker build -t gezpage/mysql mysql
+docker build -t gezpage/node node
+docker build -t gezpage/rails rails
+docker build -t gezpage/redis redis
+docker build -t gezpage/symfony2:5.3 symfony2
+docker build -t gezpage/symfony2:5.5 symfony2.php55
+
+docker images
+
+echo "Finished building docker images"
