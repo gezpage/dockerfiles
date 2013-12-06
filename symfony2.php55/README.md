581f030ebc0849243805a6d9c985b65bc877481e
Gez Page <gezpage@gmail.com>
2013-12-06 09:37:33 +0000

    Added symfony2 / php 5.5 build script

---
+++ symfony2.php55/README.md
@@ -0,0 +1,42 @@
+# Symfony2 dockerfile (PHP 5.5 Version)
+
+This will create a dev environment to run a Symfony2 project with the
+following toolset:
+
+* PHP 5.5 (PPA)
+* Apache 2.2
+* Nodejs (PPA)
+* APC (PECL)
+* SSH
+* Git
+* Curl
+* Postfix
+* Vim
+
+Included is a link to an already running mysql container, and a mounted
+folder where the project files are located.
+
+## Ports
+
+* SSH  6022
+* HTTP 6080
+
+## Build steps
+
+Build the image:
+
+    docker build -t gezpage/symfony2:php5.5 .
+
+Run a container binding a mount point to your Symfony2 project data:
+
+    docker run -d -name symfony2 -link mysql:db -p 6022:22 -p 6080:80 -v /path/to/symfony2project:/var/www gezpage/symfony2:php5.5
+
+Connect via SSH:
+
+    ssh root@localhost -p 6022
+
+## Usage
+
+You can now enter http://localhost:6080 in your web browser to access
+the website, and use the project folder to edit files on your local
+machine.
---
+++ symfony2.php55/files/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Install script for PHP Symfony2 development
+#
+
+# Install nodejs
+printf "\n\n * Installing PHP 5.5 PPA\n\n"
+add-apt-repository -y ppa:ondrej/php5
+apt-get update
+apt-get install -y php5
+
+# Install APC using PECL
+printf "\n\n * Installing APC\n\n"
+printf "\n" | pecl install apc
+echo extension=apc.so > /etc/php5/conf.d/apc.ini
+
+# Install nodejs
+printf "\n\n * Installing Nodejs PPA\n\n"
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
+
+# Enable Apache mod rewrite
+printf "\n\n * Enabling Apache mod_rewrite\n\n"
+a2enmod rewrite
+service apache2 reload
+
+# Delete default vhost
+rm /etc/apache2/sites-enabled/000-default
+
+# Install Composer globally
+printf "\n\n * Installing Composer\n\n"
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install LessCSS globally
+printf "\n\n * Installing Less CSS\n\n"
+npm install --global less
+
+printf "\n\n * Install complete!\n\n"
